8d43f537eeb56a2bc35c7558734e2842b173c9f5
daradan <daradankz@gmail.com>
2020-06-28 01:34:33 +0600

    v0.2 (20200627)

--- iedem_gui_converter.py
+++ iedem_gui_converter.py
@@ -2,51 +2,98 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import font
 from tkinter import scrolledtext
+from tkinter import messagebox
 from requests import get
-from os import remove
+from requests import exceptions
 import webbrowser
 
 
+# функция вывода changelog при нажатии на версию
+def changelog(*args):
+    messagebox.showinfo('История изменении', 'v0.1 (20200620):'
+                                             '\n- первая версия;'
+                                             '\n'
+                                             '\nv0.2 (20200627):'
+                                             '\n- оптимизировал код;'
+                                             '\n- сделал проверку правильность ссылки'
+                                             '\n- исправил ошибку, если пользователь не указывал избранные'
+                                             '\n- мелкие изменения')
+
+
+# функция для вывода сообщении о проблеме ссылки
+def msg_err_url():
+    messagebox.showinfo('Ошибка', 'Вероятные проблемы:'
+                                  '\n- Не указали ссылку;'
+                                  '\n- В указанной ссылке нет плейлиста;'
+                                  '\n- В указанной ссылке плейлист не от Iedem;'
+                                  '\n- Нет соединения с интернетом.')
+
+
+# функция для формирования списка всех каналов
 def dwnld_all_channel():
+    # качаем ссылку, если проблемы со ссылкой, то выводим ошибку
     url = mp3u_entry.get()
-    my_file = get(url)
-    open('temp.m3u8', 'wb').write(my_file.content)
+    try:
+        playlist = get(url)
+    except exceptions.MissingSchema:
+        msg_err_url()
+
+    playlist_list = []
     json_list = []
-    with open('temp.m3u8', 'r', encoding='utf-8') as f:
-        for k in f:
-            if '#EXTINF' in k:
-                json_list.append(k.split(',')[1].strip())
-    json_list.sort()
+
+    # переводим в utf8 и добавляем каждую строку в список
+    playlist.encoding = 'utf-8'
+    for playlist_line in playlist.text.split('\n'):
+        playlist_list.append(playlist_line.strip('\r'))
+
+    # находим строку с названием канала и добавлем ее в список
+    for playlist_line_2 in playlist_list:
+        if '#EXTINF' in playlist_line_2:
+            json_list.append(playlist_line_2.split(',')[1].strip())
+
+    # сортируем каналы и выводим их
     n = 1
-    for k in json_list:
+    for k in sorted(json_list):
         all_channels.insert('end', f'{n}.\t{k}\n')
         n += 1
-    remove('temp.m3u8')
 
 
+# функция, которая конвентирует и сортирует
 def convert_playlist():
+    # качаем ссылку, если проблемы со ссылкой, то выводим ошибку
     url = mp3u_entry.get()
-    my_file = get(url)
-    open('temp.m3u8', 'wb').write(my_file.content)
+    try:
+        playlist = get(url)
+    except exceptions.MissingSchema:
+        msg_err_url()
+
     json_list = []
-    temp_lst = []
-    temp = 0
-    group_del_list = []
+    playlist_list = []
+    temp_dict = {}
+    group_edited_list = []
     num_on_list = []
-    with open('temp.m3u8', 'r', encoding='utf-8') as f:
-        temp_dict = {}
-        for i in f:
-            if '#EXTINF' in i:
-                temp_dict['tvg-name'] = i.split(',')[1].strip()
-                temp_dict['tvg-rec'] = i.split(',')[0][10:]
-                temp_dict['tvg-logo'] = 'https://tv.sdckz.com/logo/' + temp_dict['tvg-name'].replace(' ', '') + '.png'
-            elif '#EXTGRP' in i:
-                temp_dict['group-title'] = i.replace('#EXTGRP:', '').strip()
-            elif 'http' in i:
-                temp_dict['link'] = i.strip()
-                json_list.append(temp_dict)
-                temp_dict = {}
-    remove('temp.m3u8')
+    group_del_list = []
+    temp_num = 0
+
+    # переводим в utf8 и добавляем каждую строку в список
+    playlist.encoding = 'utf-8'
+    for playlist_line in playlist.text.split('\n'):
+        playlist_list.append(playlist_line.strip('\r'))
+
+    # каждый необходимый "тег" добавляем в словарь
+    for playlist_line_2 in playlist_list:
+        if '#EXTINF' in playlist_line_2:
+            temp_dict['tvg-name'] = playlist_line_2.split(',')[1].strip()
+            temp_dict['tvg-rec'] = playlist_line_2.split(',')[0][10:]
+            temp_dict['tvg-logo'] = 'https://tv.sdckz.com/logo/' + temp_dict['tvg-name'].replace(' ', '') + '.png'
+        elif '#EXTGRP' in playlist_line_2:
+            temp_dict['group-title'] = playlist_line_2.replace('#EXTGRP:', '').strip()
+        elif 'http' in playlist_line_2:
+            temp_dict['link'] = playlist_line_2.strip()
+            json_list.append(temp_dict)
+            temp_dict = {}
+
+    # получаем статус checkbutton всех групп
     if group_del_state_1.get():
         group_del_list.append('Новый сайт - edemtv.me')
     if group_del_state_2.get():
@@ -97,52 +144,58 @@ def convert_playlist():
         group_del_list.append('HD Orig')
     if group_del_state_25.get():
         group_del_list.append('4K')
-    with open('temp.m3u8', 'w', encoding='utf-8') as f:
-        f.write('#EXTM3U x-tvg-url="http://epg.it999.ru/epg.xml.gz"\n')
-        for d in json_list:
-            if d['group-title'] not in group_del_list:
-                f.write(
-                    f'''#EXTINF:0 group-title="{d['group-title']}" tvg-name="{d['tvg-name']}"\
- tvg-logo="{d['tvg-logo']}" {d['tvg-rec']},{d['tvg-name']}''' + '\n')
-                f.write(d['link'] + '\n')
-    with open('temp.m3u8', 'r', encoding='utf-8') as f:
-        for k in f:
-            temp_lst.append(k)
+
+    # из вышесозданного словаря формируем новый плейлист. Также производится удаление выбранных групп (по checkbutton)
+    for playlist_line_3 in json_list:
+        if playlist_line_3['group-title'] not in group_del_list:
+            group_edited_list.append(f'''#EXTINF:0 group-title="{playlist_line_3['group-title']}"\
+ tvg-name="{playlist_line_3['tvg-name']}" tvg-logo="{playlist_line_3['tvg-logo']}"\
+ {playlist_line_3['tvg-rec']},{playlist_line_3['tvg-name']}''')
+            group_edited_list.append(playlist_line_3['link'])
+
+    # получаем список избранных каналов
     favorites_list = favorites_channels.get('1.0', END)
     favorites_list = favorites_list.split('\n')
     del favorites_list[-1]
 
-    while temp < len(favorites_list):
-        with open('temp.m3u8', 'r', encoding='utf-8') as f:
-            for num, val in enumerate(f):
-                if favorites_list[temp] in val:
-                    num_on_list.append(num)
-                    num_on_list.append(num + 1)
-            temp += 1
-    remove('temp.m3u8')
+    # формируем список избранных с получением их номера строк из вышесозданного плейлиста
+    while temp_num < len(favorites_list):
+        for num, val in enumerate(group_edited_list):
+            if favorites_list[temp_num] in val:
+                num_on_list.append(num)
+                num_on_list.append(num + 1)
+        temp_num += 1
 
-    finish_fav_list_1 = ['#EXTM3U x-tvg-url="http://epg.it999.ru/epg.xml.gz"\n']
-    for k in num_on_list:
-        finish_fav_list_1.append(temp_lst[k])
+    # создаем плейлист с избранными каналами или просто шапку, если пользователь не пожелал избранных
+    pre_finish_fav_list = ['#EXTM3U x-tvg-url="http://epg.it999.ru/epg.xml.gz"']
+    if '' not in favorites_list:
+        for k in num_on_list:
+            pre_finish_fav_list.append(group_edited_list[k])
 
-    for k in sorted(num_on_list, reverse=True):
-        del temp_lst[k]
-    del temp_lst[0]
-    finish_fav_list_both = finish_fav_list_1 + temp_lst
+    # удаляем из созданного плейлиста избранные каналы (чтобы не было дублей), и соединяем его к избранному плейлисту
+    if '' not in favorites_list:
+        for i in sorted(num_on_list, reverse=True):
+            del group_edited_list[i]
+    # del group_edited_list[0]
+    finish_fav_list_both = pre_finish_fav_list + group_edited_list
 
-    with open('pl.m3u8', 'w', encoding='utf-8') as f:
-        for k in finish_fav_list_both:
-            f.write(k)
+    # создаем конечный плейлист в файл
+    with open('pl.m3u8', 'w', encoding='utf-8') as fin_file:
+        for j in finish_fav_list_both:
+            fin_file.write(j + '\n')
 
 
+# создаем интерфейс с названием и указываем, чтобы размер окна был фиксированным
 root = Tk()
 root.title('IEDEM Converter')
 root.resizable(width=False, height=False)
 
+# создаем разметки и шрифт
 mainframe = ttk.Frame(root, borderwidth=5, width=200, height=100)
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-font_bold = font.Font(weight='bold')
+font_bold = font.Font(weight='bold', size=10)
 
+# далее идут виджеты
 ttk.Label(mainframe, text='Введите ссылку на плейлист:', font=font_bold)\
     .grid(column=0, row=0, sticky=W, columnspan=2)
 
@@ -289,13 +342,13 @@ all_channels.grid(column=0, row=11, columnspan=3)
 favorites_channels = scrolledtext.ScrolledText(mainframe, width=33, height=15)
 favorites_channels.grid(column=3, row=11, columnspan=3)
 
-
+# виджет, который при нажатии запускает функцию создания списка всех каналов
 ttk.Button(mainframe, text="Загрузить", command=dwnld_all_channel)\
     .grid(column=1, row=10, sticky=W)
 
+# виджет, который при нажатии запускает функцию конвентирования
 ttk.Button(mainframe, text='КОНВЕНТИРОВАТЬ!', command=convert_playlist)\
     .grid(column=0, row=12, sticky=(W, E), columnspan=6)
-
 
 author = ttk.Label(mainframe, text='daradan')
 author.grid(column=5, row=13, sticky=E)
@@ -305,9 +358,9 @@ donate = ttk.Label(mainframe, text='donate')
 donate.grid(column=0, row=13, sticky=W)
 donate.bind('<Button-1>', lambda event: webbrowser.open("https://www.paypal.me/daradan?locale.x=ru_RU"))
 
-version = ttk.Label(mainframe, text='v0.1(20200620)')
-version.grid(column=3, row=13, sticky=(W, E))
-
+version = ttk.Label(mainframe, text='v0.2(20200627)')
+version.grid(column=2, row=13, sticky=(W, E), columnspan=3)
+version.bind('<Button-1>', changelog)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=0, pady=2)
